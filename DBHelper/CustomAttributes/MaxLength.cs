4c22712deb431094462d69adfcc36fec55838a87
pcstx <pcstx@163.com>
2014-03-21 22:11:29 +0800

    01

---
+++ DBHelper/CustomAttributes/MaxLength.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBHelper.CustomAttributes
+{
+    /// <summary>
+    /// 最大长度
+    /// </summary>
+    public class MaxLength : Attribute
+    {
+          private int maxLength;
+          public MaxLength(int n)
+        {
+            this.maxLength = n;
+        }
+
+        public int GetMaxLength()
+        {
+            return maxLength;
+        }
+    }
+}
---
+++ DBHelper/CustomAttributes/MinLength.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DBHelper.CustomAttributes
+{
+    /// <summary>
+    /// 最小长度
+    /// </summary>
+    public class MinLength:Attribute
+    {
+        private int minLength;
+        public MinLength(int n)
+        {
+            this.minLength = n;
+        }
+
+        public int GetMinLength()
+        {
+            return minLength;
+        }
+
+    }
+}
